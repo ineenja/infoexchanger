a76de81541418070687385b529ab885a446dead7
Andrey Tasmagambetov <theiznogoud@gmail.com>
2025-03-20 16:11:09 +0300

    added more tests on pocked reading and writing

--- buffermanager/test/buffermanager_test.cpp
+++ buffermanager/test/buffermanager_test.cpp
@@ -49,8 +49,7 @@ TEST(BufferManagerTests, BufferManagerPocketsHeaderTest) {
     }
 }
 
-///// тест инициализации памяти и менеджера буфера
-TEST(BufferManagerTests, BufferManagerReadWriteOnePocketTest) {
+TEST(BufferManagerTests, BufferManagerReadWriteOnePocketOneMsgTest) {
     const std::string shmName = "MySharedMemory";
     const size_t shmSize = 1024;
     const size_t pocketSize = 10; // количество байт информации, содержащейся в одном сообщении
@@ -70,7 +69,7 @@ TEST(BufferManagerTests, BufferManagerReadWriteOnePocketTest) {
     }
     EXPECT_EQ(readData.size(), 5);
 
-    std::cout << std::endl;
+    std::cout << "One pocket one msg test is over" << std::endl << std::endl;
 }
 
 TEST(BufferManagerTests, BufferManagerReadWriteManyPocketsOneMsgTest) {
@@ -91,5 +90,130 @@ TEST(BufferManagerTests, BufferManagerReadWriteManyPocketsOneMsgTest) {
     for (size_t i = 0; i < dataToWrite.size(); ++i) {
         EXPECT_EQ(readData[i], dataToWrite[i]);
     }
+
+    std::cout << "many pockets one msg test is over" << std::endl << std::endl;
 }
 
+TEST(BufferManagerTests, BufferManagerReadWriteOnePocketManyMsgsNextToEachOtherTest) {
+    const std::string shmName = "MySharedMemory";
+    const size_t shmSize = 1024;
+    const size_t pocketSize = 10; // количество байт информации, содержащейся в одном сообщении
+
+    // Создаем менеджер буферов
+    BufferManager bufferManager(shmName, shmSize, pocketSize);
+
+    // Пример записи данных в shared memory
+    std::vector<uint8_t> dataToWrite1 = {1,2,3,4,5};
+    bufferManager.writePocket(dataToWrite1, 5);
+
+    std::vector<uint8_t> dataToWrite2 = {1,2,3,4,5,6,7};
+    bufferManager.writePocket(dataToWrite2, 6);
+
+    // Пример чтения данных из shared memory
+    std::vector<uint8_t> readData1 = bufferManager.readPocket(5);
+
+    std::vector<uint8_t> readData2 = bufferManager.readPocket(6);
+
+    for (size_t i = 0; i < dataToWrite1.size(); ++i) {
+        EXPECT_EQ(readData1[i], i+1);
+    }
+    EXPECT_EQ(readData1.size(), 5);
+
+    for (size_t i = 0; i < dataToWrite2.size(); ++i) {
+        EXPECT_EQ(readData2[i], i+1);
+    }
+    EXPECT_EQ(readData2.size(), 7);
+
+    std::cout << "One pocket many msg test next to each other is over" << std::endl << std::endl;
+}
+
+TEST(BufferManagerTests, BufferManagerReadWriteOnePocketManyMsgsSplitApartTest) {
+    const std::string shmName = "MySharedMemory";
+    const size_t shmSize = 1024;
+    const size_t pocketSize = 10; // количество байт информации, содержащейся в одном сообщении
+
+    // Создаем менеджер буферов
+    BufferManager bufferManager(shmName, shmSize, pocketSize);
+
+    // Пример записи данных в shared memory
+    std::vector<uint8_t> dataToWrite1 = {1,2,3,4,5};
+    bufferManager.writePocket(dataToWrite1, 5);
+
+    std::vector<uint8_t> dataToWrite2 = {1,2,3,4,5,6,7};
+    bufferManager.writePocket(dataToWrite2, 10);
+
+    // Пример чтения данных из shared memory
+    std::vector<uint8_t> readData1 = bufferManager.readPocket(5);
+
+    std::vector<uint8_t> readData2 = bufferManager.readPocket(10);
+
+    for (size_t i = 0; i < dataToWrite1.size(); ++i) {
+        EXPECT_EQ(readData1[i], i+1);
+    }
+    EXPECT_EQ(readData1.size(), 5);
+
+    for (size_t i = 0; i < dataToWrite2.size(); ++i) {
+        EXPECT_EQ(readData2[i], i+1);
+    }
+    EXPECT_EQ(readData2.size(), 7);
+
+    std::cout << "One pocket many msg test split apart is over" << std::endl << std::endl;
+}
+
+TEST(BufferManagerTests, BufferManagerReadWriteManyPocketsManyMsgsNextToEachOtherTest) {
+    const std::string shmName = "MySharedMemory";
+    const size_t shmSize = 1024;
+    const size_t pocketSize = 10; // количество байт информации, содержащейся в одном сообщении
+
+    // Создаем менеджер буферов
+    BufferManager bufferManager(shmName, shmSize, pocketSize);
+
+    // Пример записи данных в shared memory
+    std::vector<uint8_t> dataToWrite1 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+    bufferManager.writePocket(dataToWrite1, 5); // Записываем начиная с индекса 0
+
+    std::vector<uint8_t> dataToWrite2 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29};
+    bufferManager.writePocket(dataToWrite2, 9); // Записываем начиная с индекса 0
+
+    // Пример чтения данных из shared memory
+    std::vector<uint8_t> readData1 = bufferManager.readPocket(5);
+    for (size_t i = 0; i < dataToWrite1.size(); ++i) {
+        EXPECT_EQ(readData1[i], dataToWrite1[i]);
+    }
+
+    std::vector<uint8_t> readData2 = bufferManager.readPocket(9);
+    for (size_t i = 0; i < dataToWrite2.size(); ++i) {
+        EXPECT_EQ(readData2[i], dataToWrite2[i]);
+    }
+
+    std::cout << "many pockets many msg test next to each other is over" << std::endl << std::endl;
+}
+
+TEST(BufferManagerTests, BufferManagerReadWriteManyPocketsManyMsgsSplitApartTest) {
+    const std::string shmName = "MySharedMemory";
+    const size_t shmSize = 1024;
+    const size_t pocketSize = 10; // количество байт информации, содержащейся в одном сообщении
+
+    // Создаем менеджер буферов
+    BufferManager bufferManager(shmName, shmSize, pocketSize);
+
+    // Пример записи данных в shared memory
+    std::vector<uint8_t> dataToWrite1 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+    bufferManager.writePocket(dataToWrite1, 5); // Записываем начиная с индекса 0
+
+    std::vector<uint8_t> dataToWrite2 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29};
+    bufferManager.writePocket(dataToWrite2, 10); // Записываем начиная с индекса 0
+
+    // Пример чтения данных из shared memory
+    std::vector<uint8_t> readData1 = bufferManager.readPocket(5);
+    for (size_t i = 0; i < dataToWrite1.size(); ++i) {
+        EXPECT_EQ(readData1[i], dataToWrite1[i]);
+    }
+
+    std::vector<uint8_t> readData2 = bufferManager.readPocket(10);
+    for (size_t i = 0; i < dataToWrite2.size(); ++i) {
+        EXPECT_EQ(readData2[i], dataToWrite2[i]);
+    }
+
+    std::cout << "many pockets many msg test split apart is over" << std::endl << std::endl;
+}
