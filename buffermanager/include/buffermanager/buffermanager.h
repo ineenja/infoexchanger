f6d389c123783e668e1091e94756d785d65c00aa
Andrey Tasmagambetov <theiznogoud@gmail.com>
2025-03-20 10:57:24 +0300

    wrote createPockets foo

--- buffermanager/include/buffermanager/buffermanager.h
+++ buffermanager/include/buffermanager/buffermanager.h
@@ -8,31 +8,56 @@ namespace Bip = boost::interprocess;
 class BufferManager {
 public:
     BufferManager(const std::string& shmName, size_t shmSize, size_t offset)
-        : memorySupply(shmName, shmSize), mutex(Bip::open_or_create, "mtx"), offset(offset) {}
+        : memorySupply(shmName, shmSize), mutex(Bip::open_or_create, "mtx"), offset(offset) {
+        idPocket = 0;
+    }
 
-    void writePocket(const std::vector<uint8_t>& data, size_t position) {
+    void writePocket(const std::vector<uint8_t>& data, size_t position, size_t pocketSize) {
         if (offset * position + data.size() > memorySupply.getSize()) {
             throw std::out_of_range("Write operation exceeds shared memory size");
         }
 
         Bip::scoped_lock<Bip::named_mutex> lock(mutex);
+
         void* mem = memorySupply.getMemory();
         std::memcpy(static_cast<uint8_t*>(mem) + offset * position, data.data(), data.size());
     }
 
-    std::vector<uint8_t> readPocket(size_t size, size_t position) {
+    std::vector<uint8_t> readPocket(size_t size, size_t position, size_t pocketSize) {
         if (offset * position + size > memorySupply.getSize()) {
             throw std::out_of_range("Read operation exceeds shared memory size");
         }
 
         Bip::scoped_lock<Bip::named_mutex> lock(mutex);
+
         void* mem = memorySupply.getMemory();
         std::vector<uint8_t> buffer(size);
         std::memcpy(buffer.data(), static_cast<uint8_t*>(mem) + offset * position, size);
         return buffer;
     }
 
+    std::vector<std::vector<uint8_t>> createPockets(const std::vector<uint8_t>& data, size_t pocketSize) {
+        std::vector<std::vector<uint8_t>> pockets;
+        ++idPocket;
+
+        uint8_t numberOfPocket = 0;
+        for (size_t i = 0; i < data.size(); i++) {
+            std::vector<uint8_t> newPocket(pocketSize+2, 0); // размер вектора байт - пакета составляет pocketSize в байтах плюс 2 байта метаинформации
+            for (size_t j = 0; j < pocketSize; j++) {
+                newPocket[0] = idPocket; // pocket ID
+                newPocket[1] = numberOfPocket;; // pocketNo for particular pocket ID
+                newPocket[j+2] = data[i++];
+            }
+            i--;
+            numberOfPocket++;
+            pockets.push_back(newPocket);
+        }
+        return pockets;
+    }
+
 private:
+    uint8_t idPocket;
+
     SharedMemMemorySupplier memorySupply;
     Bip::named_mutex mutex;
     size_t offset;
--- buffermanager/test/buffermanager_test.cpp
+++ buffermanager/test/buffermanager_test.cpp
@@ -2,7 +2,7 @@
 #include "buffermanager/buffermanager.h"
 
 ///// тест инициализации памяти и менеджера буфера
-TEST(BufferManagerTests, BufferManagerTest) {
+TEST(BufferManagerTests, BufferManagerReadWriteTest) {
     const std::string shmName = "MySharedMemory";
     const size_t shmSize = 1024;
     const size_t offset = 100;
@@ -12,13 +12,50 @@ TEST(BufferManagerTests, BufferManagerTest) {
 
     // Пример записи данных в shared memory
     std::vector<uint8_t> dataToWrite = {0x01, 0x02, 0x03, 0x04, 0x05};
-    bufferManager.writePocket(dataToWrite, 5); // Записываем начиная с индекса 0
+    size_t pocketSize = 10; // количество байт информации, содержащейся в одном сообщении
+    bufferManager.writePocket(dataToWrite, 5, pocketSize); // Записываем начиная с индекса 0
 
     // Пример чтения данных из shared memory
-    std::vector<uint8_t> read_data = bufferManager.readPocket(dataToWrite.size(), 5);
+    std::vector<uint8_t> read_data = bufferManager.readPocket(dataToWrite.size(), 5, pocketSize);
     int i = 1;
     for (uint8_t byte : read_data) {
         EXPECT_EQ(static_cast<int>(byte), i++);
     }
     std::cout << std::endl;
+}
+
+TEST(BufferManagerTests, BufferManagerPocketsCreationTest) {
+    const std::string shmName = "MySharedMemory";
+    const size_t shmSize = 1024;
+    const size_t offset = 100;
+
+    // Создаем менеджер буферов
+    BufferManager bufferManager(shmName, shmSize, offset);
+
+    std::vector<uint8_t> dataToWrite = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+
+    size_t pocketSize = 10; // количество байт информации, содержащейся в одном сообщении
+    std::vector<std::vector<uint8_t>> createdPockets = bufferManager.createPockets(dataToWrite, pocketSize);
+
+    EXPECT_EQ(createdPockets.size(), 4);
+}
+
+TEST(BufferManagerTests, BufferManagerPocketsHeaderTest) {
+    const std::string shmName = "MySharedMemory";
+    const size_t shmSize = 1024;
+    const size_t offset = 100;
+
+    // Создаем менеджер буферов
+    BufferManager bufferManager(shmName, shmSize, offset);
+
+    std::vector<uint8_t> dataToWrite = {11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+
+    size_t pocketSize = 10; // количество байт информации, содержащейся в одном сообщении
+    std::vector<std::vector<uint8_t>> createdPockets = bufferManager.createPockets(dataToWrite, pocketSize);
+
+    for (size_t i = 0; i < createdPockets.size(); ++i) {
+        EXPECT_EQ(createdPockets[i][0], 1);
+        EXPECT_EQ(createdPockets[i][1], i);
+    }
+
 }
